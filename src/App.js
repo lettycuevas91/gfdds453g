4b17a2968435d00c587760d5c0d199c52e626960
Leticia <lettycuevas91@gmail.com>
2023-01-20 22:19:21 -0300

    update

--- src/App.js
+++ src/App.js
@@ -11,30 +11,23 @@ import {
 
 
 export class App extends Component {
-
-
   render() {
-   
     return (
-      <div>
-      <nav>
-      <Link to="page1">Page1</Link>
-      <Link to="page2">Page2</Link>      
-      </nav>
-      <Switch>
-      <Route path="/" exact component={Page1} />
-      <Route path="/page1" component={Page1} />
-      <Route path="/page2" component={Page2} />
-      <Route component={NotFound}/>
-      </Switch>
-    </div>
+         <div>
+        <nav>
+        <Link to="page1">Page1</Link>
+        <Link to="page2">Page2</Link>      
+        </nav>
+        <Switch>
+        <Route path="/" exact component={Page1} />
+        <Route path="/page1" component={Page1} />
+        <Route path="/page2" component={Page2} />
+        <Route component={NotFound}/>
+        </Switch>
+      </div>
     )
   }
 }
-
-// No tienes que hacer nada por debajo de esta linea
-//==================================================
-
 
 export const Page1 = (props) => {
   return (
